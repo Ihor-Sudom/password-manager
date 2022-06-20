bcd2b351e343b0453c05a47202512ca3f84e7618
Ihor-Sudom <ihor.sudom@gmail.com>
2022-06-20 10:58:13 +0300

    layout test

---
+++ src/layout/__tests__/layout.spec.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Layout from '../index.tsx'
+import Login from '../../components/containers/login/Login.tsx'
+import { LoginContainerStyled } from '../../components/containers/login/Login.styles.ts'
+
+const wrapperLayout = (props) => shallow(<Layout {...props} />)
+const wrapperLogin = () => shallow(<Login />)
+const componentLogin = wrapperLogin()
+
+const props = { children: componentLogin }
+
+const component = wrapperLayout(props)
+
+describe('Layout component', () => {
+  it('Should render Layout with children property', () => {
+    expect(component.find(LoginContainerStyled).length).toBe(1)
+  })
+})
